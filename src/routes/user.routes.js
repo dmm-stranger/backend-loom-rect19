3997e8d9c8ca7fb244a5b0705ead080e22db7e01
dmm.stranger <molla.dmm@gmail.com>
2026-08-22 22:32:06 +0600

    problem fixed: category product slug problem send front in a string but they need object. cast error found and fixed

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { updateProfile, getAddresses, addAddress, updateAddress, deleteAddress } from "../controllers/user.controller.js";
+import { updateProfile, getAddresses, addAddress, updateAddress, deleteAddress, deleteAccount } from "../controllers/user.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
 import { upload } from "../middleware/upload.middleware.js";
 
 const router = express.Router();
 router.use(protect);
 router.patch("/profile", upload.single("image"), updateProfile);
+router.delete("/account", deleteAccount);
 router.get("/addresses", getAddresses);
 router.post("/addresses", addAddress);
 router.patch("/addresses/:addressId", updateAddress);
